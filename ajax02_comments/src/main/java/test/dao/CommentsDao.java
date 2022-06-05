54fd1d37dbef0f47da0f6d114f3762935cd3048f
RadiantK <pps8853@gmail.com>
2022-06-05 23:25:32 +0900

    CommentsDao 생성
    싱글톤형식으로 객체의 인스턴스 생성

---
+++ ajax02_comments/src/main/java/test/dao/CommentsDao.java
@@ -0,0 +1,102 @@
+package test.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import test.dto.Comments;
+import test.util.DBPool;
+
+public class CommentsDao {
+
+	private static CommentsDao commentsDao = null;
+	
+	private CommentsDao() {}
+	
+	public static CommentsDao getInstance() {
+		if(commentsDao == null) {
+			commentsDao = new CommentsDao();
+		}
+		return commentsDao;
+	}
+	
+	public List<Comments> selectList(int reqNum) {
+		String sql = "SELECT * FROM comments WHERE bnum = ?";
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		List<Comments> list = new ArrayList<>();
+		
+		try {
+			con = DBPool.getConnection();
+			pstmt = con.prepareStatement(sql);
+			pstmt.setInt(1, reqNum);
+			rs = pstmt.executeQuery();
+			
+			while(rs.next()) {
+				int num = rs.getInt("num");
+				int bnum = rs.getInt("bnum");
+				String id = rs.getString("id");
+				String comments = rs.getString("comments");
+				
+				Comments comm = new Comments(num, bnum, id, comments);
+				list.add(comm);
+			}
+			
+			return list;
+		}catch(SQLException e) {
+			e.printStackTrace();
+			return null;
+		}finally {
+			DBPool.close(con, pstmt, rs);
+		}
+	}
+	
+	public int insert(Comments co) {
+		String sql = "INSERT INTO comments(bnum, id, comments) "
+				+ "VALUES(?,?,?)";
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		
+		try {
+			con = DBPool.getConnection();
+			pstmt = con.prepareStatement(sql);
+			pstmt.setInt(1, co.getBnum());
+			pstmt.setString(2, co.getId());
+			pstmt.setString(3, co.getComments());
+			
+			int result = pstmt.executeUpdate();
+			
+			return result;
+		}catch(SQLException e) {
+			e.printStackTrace();
+			return -1;
+		}finally {
+			DBPool.close(con, pstmt);
+		}
+	}
+	
+	public int delete(int num) {
+		String sql = "DELETE FROM comments WHERE num = ?";
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		
+		try {
+			con = DBPool.getConnection();
+			pstmt = con.prepareStatement(sql);
+			pstmt.setInt(1, num);
+			
+			int result = pstmt.executeUpdate();
+			
+			return result;
+		}catch(SQLException e) {
+			e.printStackTrace();
+			return -1;
+		}finally {
+			DBPool.close(con, pstmt);
+		}
+	}
+}
